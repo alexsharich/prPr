e01787a8842a28e5fe7b23d439f5f0dbbd58358c
alexsharich <alexsharich@gmail.com>
2023-03-03 14:33:58 +0100

    change db.json, add tests for loginSlice, add tests for selectors, add testAsyncThunk

---
+++ src/features/AuthByUsername/model/slice/loginSlice.test.ts
@@ -0,0 +1,21 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { loginByUsername } from '../services/loginByUsername/loginByUsername';
+import { LoginSchema } from '../types/loginSchema';
+import { loginActions, loginReducer } from './loginSlice';
+
+describe('loginSlice', () => {
+    test('loginSlice test set username', () => {
+        const state: DeepPartial<LoginSchema> = { username: '123' };
+        expect(loginReducer(
+          state as LoginSchema,
+          loginActions.setUserName('123'),
+        )).toEqual({ username: '123' });
+    });
+    test('loginSlice test set password', () => {
+        const state: DeepPartial<LoginSchema> = { password: '123' };
+        expect(loginReducer(
+          state as LoginSchema,
+          loginActions.setUserPassword('123'),
+        )).toEqual({ password: '123' });
+    });
+});
