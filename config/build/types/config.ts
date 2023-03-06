adfc8968e51e16dcb1d03b30d315207fd4c37e2f
alexsharich <alexsharich@gmail.com>
2023-03-06 10:47:56 +0100

    add api instance

--- config/build/types/config.ts
+++ config/build/types/config.ts
@@ -10,11 +10,13 @@ export interface BuildPaths {
 export interface BuildEnv {
   mode: BuildMode
   port: number
+  apiUrl: string
 }
 
 export interface BuildOptions {
-  mode: BuildMode,
+  mode: BuildMode
   paths: BuildPaths
   isDev: boolean
   port: number
+  apiUrl: string
 }
--- src/app/providers/StoreProvider/config/StateSchema.ts
+++ src/app/providers/StoreProvider/config/StateSchema.ts
@@ -1,10 +1,13 @@
 import {
     AnyAction, CombinedState, EnhancedStore, Reducer, ReducersMapObject,
 } from '@reduxjs/toolkit';
+import { AxiosInstance } from 'axios';
 import { CounterSchema } from 'entities/Counter';
 import { ProfileSchema } from 'entities/Profile';
 import { UserSchema } from 'entities/User';
 import { LoginSchema } from 'features/AuthByUsername';
+import { NavigateOptions } from 'react-router-dom';
+import { To } from 'history';
 
 export interface StateSchema {
  counter:CounterSchema
@@ -24,4 +27,12 @@ export interface ReducerManager {
 
 export interface ReduxStoreWithManager extends EnhancedStore<StateSchema> {
   reducerManager: ReducerManager
+}
+export interface ThunkExtraArg {
+  api: AxiosInstance
+  navigate:(to:To, options?:NavigateOptions)=>void
+}
+export interface ThunkConfig<T>{
+  rejectValue:T
+  extra:ThunkExtraArg
 }
