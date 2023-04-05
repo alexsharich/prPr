f9a06b6e7d0745ce19cefc5826687ed1accc797e
alexsharich <alexsharich@gmail.com>
2023-04-05 22:35:04 +0200

    fix: change lazy loading components

--- src/pages/ProfilePage/ui/ProfilePage.async.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.async.tsx
@@ -1,10 +1,3 @@
 import { lazy } from 'react';
 
-export const ProfilePageAsync = lazy(
-    () => new Promise((resolve) => {
-        setTimeout(() => {
-            // @ts-ignore
-            resolve(import('./ProfilePage'));
-        }, 1000);
-    }),
-);
+export const ProfilePageAsync = lazy(() => import('./ProfilePage'));
