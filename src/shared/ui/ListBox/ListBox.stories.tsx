f332420da34382a9ee41ef5d4d394d290b05f0d1
alexsharich <alexsharich@gmail.com>
2023-05-10 13:10:58 +0200

    feat: add Dropdown

--- src/shared/ui/ListBox/ListBox.stories.tsx
+++ src/shared/ui/ListBox/ListBox.stories.tsx
@@ -9,11 +9,24 @@ export default {
     argTypes: {
         backgroundColor: { control: 'color' },
     },
+    decorators: [
+        (Story) => (
+            <div style={{ padding: 100 }}>
+                <Story />
+            </div>
+        ),
+    ],
 } as ComponentMeta<typeof ListBox>;
 
 const Template: ComponentStory<typeof ListBox> = (args) => <ListBox {...args} />;
 
 export const Normal = Template.bind({});
 Normal.args = {
-
+    direction: 'bottom left',
+    value: '123',
+    items: [
+        { content: 'djfgndfjg', value: '123123' },
+        { content: 'dfgdfg', value: '12312er3' },
+        { content: 'jghjg', value: 'sdfsdf' },
+    ],
 };
