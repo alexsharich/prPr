1f0c001d360f75f738045b3086515420e5331f90
alexsharich <alexsharich@gmail.com>
2023-05-25 17:44:28 +0200

    change Select

--- src/shared/ui/Select/Select.tsx
+++ src/shared/ui/Select/Select.tsx
@@ -1,42 +1,37 @@
-import React, { ChangeEvent, memo, useMemo } from 'react';
+import React, { ChangeEvent, useMemo } from 'react';
 import { classNames, Mods } from '@/shared/lib/classNames/classNames';
 import cls from './Select.module.scss';
 
-export interface SelectOption {
-  value:string
-  content:string
+export interface SelectOption<T extends string> {
+    value: T;
+    content: string;
 }
 
-interface SelectProps {
-  className?: string
-  label?:string
-  options?:SelectOption[]
-  value?:string
-  onChange?:(value:string)=>void
-  readonly?:boolean
+interface SelectProps<T extends string> {
+    className?: string;
+    label?: string;
+    options?: SelectOption<T>[];
+    value?: T;
+    onChange?: (value: T) => void;
+    readonly?: boolean;
 }
 
-export const Select = memo((props: SelectProps) => {
-    const {
-        className,
-        label,
-        options,
-        value,
-        onChange,
-        readonly,
-    } = props;
+export const Select = <T extends string>(props: SelectProps<T>) => {
+    const { className, label, options, value, onChange, readonly } = props;
 
-    const optionsList = useMemo(() => options?.map((opt) => (
-        <option className={cls.option} value={opt.value} key={opt.value}>
-            {opt.content}
-        </option>
-    )), [options]);
+    const optionsList = useMemo(
+        () =>
+            options?.map((opt) => (
+                <option className={cls.option} value={opt.value} key={opt.value}>
+                    {opt.content}
+                </option>
+            )),
+        [options],
+    );
 
-    const mods:Mods = {
-
-    };
-    const onChangeHandler = (e:ChangeEvent<HTMLSelectElement>) => {
-        onChange?.(e.target.value);
+    const mods: Mods = {};
+    const onChangeHandler = (e: ChangeEvent<HTMLSelectElement>) => {
+        onChange?.(e.target.value as T);
     };
 
     return (
@@ -52,4 +47,4 @@ export const Select = memo((props: SelectProps) => {
             </select>
         </div>
     );
-});
+};
