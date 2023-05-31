1feaa6815d0435c263291b8b78cb2696552843ea
alexsharich <alexsharich@gmail.com>
2023-05-31 21:43:30 +0200

    upgrade storybook

--- src/features/articleRating/ui/ArticleRating/ArticleRating.stories.tsx
+++ src/features/articleRating/ui/ArticleRating/ArticleRating.stories.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import withMock from 'storybook-addon-mock';
 import ArticleRating from './ArticleRating';
 import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
 
 export default {
     title: 'features/ArticleRating',
     component: ArticleRating,
-    argTypes: {
-        decorators: [withMock],
-    },
+    argTypes: {},
 } as ComponentMeta<typeof ArticleRating>;
 
 const Template: ComponentStory<typeof ArticleRating> = (args) => <ArticleRating {...args} />;
