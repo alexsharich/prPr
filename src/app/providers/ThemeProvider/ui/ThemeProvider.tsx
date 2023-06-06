0ec812c8778d0d4f25aaab724987641061e97696
alexsharich <alexsharich@gmail.com>
2023-06-06 13:19:32 +0200

    add jsonSettings, change db-json

--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -1,19 +1,25 @@
-import React, { ReactNode, useMemo, useState } from 'react';
-import { LOCAL_STORAGE_THEME_KEY } from '@/shared/const/localstorage';
+import React, { ReactNode, useEffect, useMemo, useState } from 'react';
 import { Theme } from '@/shared/const/theme';
 import { ThemeContext } from '@/shared/lib/context/ThemeContext';
+import { useJsonSettings } from '@/entities/User';
 
 interface ThemeProviderProps {
     initialTheme?: Theme;
     children: ReactNode;
 }
 
-const defaultTheme =
-    (localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme) || Theme.LIGHT;
-
 const ThemeProvider = (props: ThemeProviderProps) => {
     const { children, initialTheme } = props;
+    const [isThemeInited, setThemeInited] = useState(false);
+    const { theme: defaultTheme = Theme.LIGHT } = useJsonSettings();
     const [theme, setTheme] = useState<Theme>(initialTheme || defaultTheme);
+
+    useEffect(() => {
+        if (!isThemeInited) {
+            setTheme(defaultTheme);
+            setThemeInited(true);
+        }
+    }, [isThemeInited, defaultTheme]);
 
     const defaultProps = useMemo(
         () => ({
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -2,21 +2,11 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Page } from '@/widgets/Page';
-import { RatingCard } from '@/entities/Rating';
 
 const MainPage = () => {
     const { t } = useTranslation('main');
 
-    return (
-        <Page data-testid="MainPage">
-            {t('main page')}
-            <RatingCard
-                title="How is article ?"
-                feedbackTitle="Your opinion"
-                hasFeedback
-            />
-        </Page>
-    );
+    return <Page data-testid="MainPage">{t('main page')}</Page>;
 };
 
 export default MainPage;
