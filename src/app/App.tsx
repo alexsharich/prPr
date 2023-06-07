90b271c542fb2c2f6080f668b021edc76b1b2094
alexsharich <alexsharich@gmail.com>
2023-06-07 16:48:44 +0200

    add layouts, appLogo, mainLayout

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,17 @@
 import React, { Suspense, useEffect } from 'react';
-import './styles/index.scss';
 import { useSelector } from 'react-redux';
 import { classNames } from '@/shared/lib/classNames/classNames';
+import { getUserInited, initAuthData } from '@/entities/User';
 import { Navbar } from '@/widgets/Navbar';
 import { Sidebar } from '@/widgets/Sidebar';
-import '@/shared/config/i18n/i18n';
-import { getUserInited, initAuthData } from '@/entities/User';
-import AppRouter from './providers/router';
 import { useTheme } from '@/shared/lib/hooks/useTheme/useTheme';
 import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch';
 import { PageLoader } from '@/widgets/PageLoader';
+import { ToggleFeatures } from '@/shared/lib/features';
+import { MainLayout } from '@/shared/layouts/MainLayout';
+import AppRouter from './providers/router';
 
-export const App = () => {
+function App() {
     const { theme } = useTheme();
     const dispatch = useAppDispatch();
     const inited = useSelector(getUserInited);
@@ -25,15 +25,32 @@ export const App = () => {
     }
 
     return (
-        <div className={classNames('app', {}, [theme])}>
-            <Suspense fallback="">
-                <Navbar />
-
-                <div className="content-page">
-                    <Sidebar />
-                    {inited && <AppRouter />}
+        <ToggleFeatures
+            feature="isAppRedesigned"
+            off={
+                <div className={classNames('app', {}, [theme])}>
+                    <Suspense fallback="">
+                        <Navbar />
+                        <div className="content-page">
+                            <Sidebar />
+                            <AppRouter />
+                        </div>
+                    </Suspense>
                 </div>
-            </Suspense>
-        </div>
+            }
+            on={
+                <div className={classNames('app_redesigned', {}, [theme])}>
+                    <Suspense fallback="">
+                        <MainLayout
+                            header={<Navbar />}
+                            content={<AppRouter />}
+                            sidebar={<Sidebar />}
+                        />
+                    </Suspense>
+                </div>
+            }
+        />
     );
-};
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from '@/app/providers/ThemeProvider';
 import './app/styles/index.scss';
 import { StoreProvider } from '@/app/providers/StoreProvider';
 import { ErrorBoundary } from '@/app/providers/ErrorBoundary';
-import { App } from './app/App';
+import App from './app/App';
 
 const container = document.getElementById('root');
 
